e7d4d395460cdbde654d8b0cb73030c644de47d8
vince.agbayani <jvinceagbayani@gmail.com>
2018-02-17 10:25:19 +0800

    dashboard fix

--- app/Http/Controllers/LevelingController.php
+++ app/Http/Controllers/LevelingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\TrainingSession;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Input;
@@ -19,29 +20,36 @@ Class LevelingController extends Controller
 		
 		$current_user = Auth::user(); 
 		$dept = $current_user->department;
-		$mg = $current_user->manager_check;
+		$mg = $current_user->manager_check;	
+		$users = User::all();
+		$training_sessions = TrainingSession::all();
 
 		
 		
 		if($current_user!=NULL)
 		{
-			if($dept=="Human Resources")
+			if($dept=="Human Resources" OR $dept==2)
 			{
 				if($mg==1)
 				{
-					return view('index_hg');
+					return view('index_hg')
+						->with('users', $users)
+						->with('training_sessions', $training_sessions);
 				}
 				else
 				{
-					return view('index_hr');
-				}
+					return view('index_hr')
+						->with('users', $users)
+						->with('training_sessions', $training_sessions);
+				}	
 
 			}
 			else
 			{
 				if($mg==1)
 				{
-					return view('index_mg');
+					return view('index_mg')
+						->with('users', $users);
 				}
 				else
 				{
