8a3193533ef9abd788363656c2aafd40ee2fa5b1
Stephen Wenceslao <stephenwenceslao@gmail.com>
2018-02-19 20:02:00 +0800

    Front end for positions CRUD

--- resources/views/positions/create.blade.php
+++ resources/views/positions/create.blade.php
@@ -1,27 +1,31 @@
 @extends('templates.dashboard-master')
 
 @section('body')
-<div class="container">
+	<main class="container create-page">
+		<section class="row crud-page-top">
+			<h1 class="crud-page-title">Add Position</h1>
+		</section>
+		<section>
+			<!-- if there are creation errors, they will show here -->
+			{{ Html::ul($errors->all()) }}
+
+			{{ Form::open(array('url' => 'positions')) }}
+				<div class="form-group">
+				    {{ Form::label('name', 'Positon Name') }}
+				    {{ Form::text('name', Request::old('name'), array('class' => 'form-control')) }}
+				</div>
+			   	<div class="form-group text-center create-bottom-wrapper">
+			   		<a href="{{ URL::to('positions') }}" class="btn cancel-btn">Cancel</a>
+			   		 {{ Form::submit('Create position', array('class' => 'btn btn-primary create-btn text-center')) }}
+			   	</div>
+
+			{{ Form::close() }}
+		</section>
+	</main>
 
 
 
-<h1>Add Position</h1>
 
-<!-- if there are creation errors, they will show here -->
-{{ Html::ul($errors->all()) }}
 
-{{ Form::open(array('url' => 'positions')) }}
 
-    <div class="form-group">
-        {{ Form::label('name', 'Positon Name') }}
-        {{ Form::text('name', Request::old('name'), array('class' => 'form-control')) }}
-    </div>
-
-  
-
-    {{ Form::submit('Add Position', array('class' => 'btn btn-primary')) }}
-
-{{ Form::close() }}
-
-</div>
 @endsection
