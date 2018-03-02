4caf1394e8f76e418c97b4a71d6015b8d7e309b2
Stephen Wenceslao <stephenwenceslao@gmail.com>
2018-03-02 14:47:42 +0800

    Styled assessment items

--- resources/views/assessment_items/edit.blade.php
+++ resources/views/assessment_items/edit.blade.php
@@ -1,30 +1,42 @@
 @extends('templates.dashboard-master')
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
+<script type="text/javascript">
+    $(document).ready(function() {
+        var a = document.getElementById('assessments');
+        a.classList.toggle("active");
+    });
+
+    // enables Bootstrap tooltips
+    $(function () {
+      $('[data-toggle="tooltip"]').tooltip();
+    });
+
+</script>
+
 @section('body')
+	<main class="container create-page">
+		<section class="row crud-page-top">
+			<h1 class="crud-page-title">Edit {{ $assessment->criteria }}</h1>
+		</section>
+		<section>
+			<!-- if there are creation errors, they will show here -->
+			{{ Html::ul($errors->all()) }}
 
-<h1>Edit {{ $assessment->criteria }} </h1>
+			{{ Form::model($assessment, array('route' => array('assessments.assessment_items.update', $assessment->id, $assessment_item->id ), 'method' => 'PUT')) }}
 
-<!-- if there are creation errors, they will show here -->
-{{ Html::ul($errors->all()) }}
+			    <div class="form-group">
+			        {{ Form::label('criteria', 'Criteria') }}
+			        {{ Form::text('criteria', Request::old('criteria'), array('class' => 'form-control', 'autofocus')) }}
+			    </div>
+				
+				<div class="form-group text-center create-bottom-wrapper">
+                    <a href="{{ URL::to('assessments/'.$assessment->id.'/assessment_items') }}" class="btn cancel-btn">Cancel</a>
+                    {{ Form::submit('Save changes', array('class' => 'btn btn-primary create-btn text-center')) }}
+                </div>
 
-<!--
-
-
--->
-
-
-{{ Form::model($assessment, array('route' => array('assessments.assessment_items.update', $assessment->id, $assessment_item->id ), 'method' => 'PUT')) }}
-
-
-    <div class="form-group">
-        {{ Form::label('criteria', 'Criteria') }}
-        {{ Form::text('criteria', Request::old('criteria'), array('class' => 'form-control')) }}
-    </div>
-
-    {{ Form::submit('Edit this Assessment Item!', array('class' => 'btn btn-primary')) }}
-
-{{ Form::close() }}
-
+			{{ Form::close() }}
+		</section>
+	</main>
 
 @endsection
--- resources/views/assessment_items/index.blade.php
+++ resources/views/assessment_items/index.blade.php
@@ -1,57 +1,86 @@
 @extends('templates.dashboard-master')
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
+<script type="text/javascript">
+    $(document).ready(function() {
+        var a = document.getElementById('assessments');
+        a.classList.toggle("active");
+    });
+
+    // enables Bootstrap tooltips
+    $(function () {
+      $('[data-toggle="tooltip"]').tooltip();
+    });
+
+</script>
+
 @section('body')
+    <main class="container-fluid">
+        <section class="container-fluid">
+            <div class="row crud-page-top">
+                <h1 class="crud-page-title">Assessment for: {{ $assessment->topic }}</h1>
+                <button>
+                    <a href="{{ URL::to('assessments/'.$assessment->id.'/assessment_items/create') }}">Add Assessment Item</a>
+                </button>
+            </div>
 
-<h2> Showing assessment_items of this assessment ({{ $assessment->topic }} ) </h2>
+            <!-- will be used to show any messages -->
+        @if (Session::has('message'))
+            <div class="alert alert-info">{{ Session::get('message') }}</div>
+        @endif
 
-<!-- will be used to show any messages -->
-@if (Session::has('message'))
-    <div class="alert alert-info">{{ Session::get('message') }}</div>
-@endif
+         
+         <br>
+         <br>
 
- <a href="{{ URL::to('assessments/'.$assessment->id.'/assessment_items/create') }}" style="float: right;">Add a Assessment Item</a>
- <br>
- <br>
+            <table class="table table-striped table-bordered">
+            <thead>
+                <tr>
+                    <td class="no-stretch">Assessment ID</td>
+                    <td class="no-stretch">Assessment Item ID</td>
+                    <td>Criteria </td>
+                    <td class="no-stretch">Actions</td>
+                </tr>
+            </thead>
 
-    <table class="table table-striped table-bordered">
-    <thead>
-        <tr>
-            <td>Assessment ID</td>
-            <td>Assessment Item ID</td>
-            <td>Criteria </td>
-        </tr>
-    </thead>
+            <tbody>
+            @foreach($assessment_items as $key => $value)
+                <tr>
+                    <td>{{ $value->assessment_id }}</td>
+                    <td>{{ $value->id }}</td>
+                    <td>{{ $value->criteria }}</td>
 
-    <tbody>
-    @foreach($assessment_items as $key => $value)
-        <tr>
-            <td>{{ $value->assessment_id }}</td>
-            <td>{{ $value->id }}</td>
-            <td>{{ $value->criteria }}</td>
+                    <!-- we will also add show, edit, and delete buttons -->
+                    <td class="table-actions">
 
-            <!-- we will also add show, edit, and delete buttons -->
-            <td>
+                        <!-- show the assessment (uses the show method found at GET /assessments/{id} -->
+                        <!-- -->
+                        <a class="btn show-btn" data-toggle="tooltip" data-placement="bottom" title="View this item" href="{{ URL::to('assessments/'.$assessment->id.'/assessment_items/'.$value->id) }}">
+                            <i class="fa fa-user fa-lg"></i>
+                        </a>
+                       
+                        <!-- edit this assessment (uses the edit method found at GET /assessments/{id}/edit -->
+                        <!-- -->
+                        <a class="btn edit-btn" data-toggle="tooltip" data-placement="bottom" title="Edit this item" href="{{ URL::to('assessments/'.$assessment->id.'/assessment_items/'.$value->id.'/edit') }}">
+                            <i class="fa fa-pencil fa-lg"></i>
+                        </a>
 
-                <!-- delete the assessment (uses the destroy method DESTROY /assessments/{id} -->
-                <!-- we will add this later since its a little more complicated than the other two buttons -->
-                    {{ Form::open(array('url' => 'assessments/'.$assessment->id.'/assessment_items/' . $value->id, 'class' => 'pull-right')) }}
-                    {{ Form::hidden('_method', 'DELETE') }}
-                    {{ Form::submit('Remove', array('class' => 'btn btn-warning')) }}
-                 {{ Form::close() }}
-                <!-- show the assessment (uses the show method found at GET /assessments/{id} -->
-                <!-- -->
-                <a class="btn btn-small btn-success" href="{{ URL::to('assessments/'.$assessment->id.'/assessment_items/'.$value->id) }}">Show this Assessment Item</a>
-               
-                <!-- edit this assessment (uses the edit method found at GET /assessments/{id}/edit -->
-                <!-- -->
-                <a class="btn btn-small btn-info" href="{{ URL::to('assessments/'.$assessment->id.'/assessment_items/'.$value->id.'/edit') }}">Edit this Assessment Item</a>
-                
-            </td>
-        </tr>
-    @endforeach
-    </tbody>
-    </table>
-   
+                        <!-- delete the assessment (uses the destroy method DESTROY /assessments/{id} -->
+                        <!-- we will add this later since its a little more complicated than the other two buttons -->
+                            {{ Form::open(array('url' => 'assessments/'.$assessment->id.'/assessment_items/' . $value->id, 'class' => 'pull-right')) }}
+                            {{ Form::hidden('_method', 'DELETE') }}
+                            <div data-toggle="tooltip" data-placement="bottom" title="Delete this item" >
+                                {{ Form::button('<i class="fa fa-trash-o fa-lg"></i>', array('type' => 'submit', 'class' => 'btn delete-btn')) }}
+                            </div>
+                         {{ Form::close() }}
+                        
+                        
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+            </table>
+        </section>
+    </main>
 
 @endsection
