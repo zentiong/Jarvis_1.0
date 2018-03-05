a7f9e8a9e02b8a3aa2707694d80aef7d272acd3c
Anferny Vanta <vantanferny@gmail.com>
2018-03-05 19:35:57 +0800

    Fixed Something with add training

--- resources/views/trainings/index.blade.php
+++ resources/views/trainings/index.blade.php
@@ -86,7 +86,7 @@
           <div class="modal-dialog" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Add New Skill</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Add New Training Session</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
