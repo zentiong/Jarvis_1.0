bc6ace33c324acd4352c10ce2e066304c6c2e721
vince.agbayani <jvinceagbayani@gmail.com>
2018-03-22 10:48:35 +0800

    added overall skills graph

--- resources/views/index_hr.blade.php
+++ resources/views/index_hr.blade.php
@@ -32,6 +32,130 @@
             <div class="row dashboard-body tabcontent" id="non-personal">
                 <div class="col-md-7">
                     <h5 class="dashboard-header">Overall skills statistics</h5>
+                        <?php
+                        $cwide_score_data = array();
+                        $cwide_label_data = array();
+                        $cwide_skill_id = array();
+                        foreach($user_skills as $key=>$value)
+                        {
+                            if(!in_array($value->skill_id, $skill_id_data))
+                            {
+                                array_push($cwide_skill_id,$value->skill_id);
+                            }
+                        }
+                        
+                        foreach($cwide_skill_id as $key=>$value)
+                        {
+                            $skill_id = $value;
+                            for($i=0;$i<sizeof($user_skills);$i++)
+                            {
+                                if($skill_id==$value->skill_id)
+                                {
+                                    if($i<sizeof($cwide_score_data))
+                                    {
+                                        $cwide_score_res = ($value->q_score/$value->q_score_max)*100;
+                                        array_push($cwide_score_data,$cwide_score_res);
+                                    }
+                                    else
+                                    {
+                                        $cwide_score_res = ($value->q_score/$value->q_score_max)*100;
+                                        $cwide_score_data[$i]+=$cwide_score_res;
+
+                                    }
+
+                                }
+                            }    
+
+                        }
+
+                        
+                        foreach($cwide_skill_id as $key => $value)
+                        {
+                            $sk_id = $value;
+                            foreach($skills as $key => $value)
+                            {
+                                if($sk_id==$value->id)
+                                {
+                                    array_push($cwide_label_data,$value->name);
+                                }
+                            }
+                        }
+
+        
+                        ?>
+
+                        <canvas id="cwide_skills_chart" width=100></canvas>
+                        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
+                        <script type="text/javascript">
+
+                            var score_data_all = <?php echo json_encode($cwide_score_data)?>;
+                            var labels_all = <?php echo json_encode($cwide_label_data)?>;
+                            var tfive = [];
+                            if(score_data_all.length>5)
+                            {
+                                tfive = score_data_all.slice(0,5);
+                            }
+                            else
+                            {
+                                tfive = score_data_all;
+                            }
+
+
+                            function update_data(chart, data) 
+                            {
+                                chart.data.datasets[0].data = data;
+                                chart.update();
+                            }
+
+
+                            Chart.defaults.global.maintainAspectRatio = false;
+                            var ctx = document.getElementById("cwide_skills_chart").getContext('2d');
+                            var myChart = new Chart(ctx, {
+                                type: 'horizontalBar',
+                                data: {
+                                    labels: labels_all,
+                                    datasets: [{
+                                        label: 'Skill Level by Percentage',
+                                        data: score_data_all,
+                                        backgroundColor: [
+                                            'rgba(255, 99, 132, 0.2)',
+                                            'rgba(54, 162, 235, 0.2)',
+                                            'rgba(255, 206, 86, 0.2)',
+                                            'rgba(75, 192, 192, 0.2)',
+                                            'rgba(153, 102, 255, 0.2)',
+                                            'rgba(255, 159, 64, 0.2)'
+                                        ],
+                                        borderColor: [
+                                            'rgba(255,99,132,1)',
+                                            'rgba(54, 162, 235, 1)',
+                                            'rgba(255, 206, 86, 1)',
+                                            'rgba(75, 192, 192, 1)',
+                                            'rgba(153, 102, 255, 1)',
+                                            'rgba(255, 159, 64, 1)'
+                                        ],
+                                        borderWidth: 1
+                                    }]
+                                },
+                                options: {
+                                    scales: {
+                                        yAxes: [{
+                                            ticks: {
+                                                beginAtZero:true
+                                            }
+                                        }],
+                                        xAxes: [{
+                                            ticks: {
+                                                beginAtZero:true
+                                            }
+                                        }]
+
+                                    }
+                                }
+                            });
+                        </script>
+
+
+
                     <div class="dashboard-content">
                     </div>
                 </div>
