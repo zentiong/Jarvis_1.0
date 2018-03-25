95f46c63644458654015c926f224106b99bb41a8
Zen Tiongson <zentiong@gmail.com>
2018-03-25 13:57:36 +0800

    fix search query

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -19,17 +19,16 @@
               tr = table.getElementsByTagName("tr");
               for (i = 0; i < tr.length; i++) 
               {
-                td = tr[i].getElementsByTagName("td")[1];
-                td1 = tr[i].getElementsByTagName("td")[2];
-                td2 = tr[i].getElementsByTagName("td")[3];
-                td3 = tr[i].getElementsByTagName("td")[4];
-                td4 = tr[i].getElementsByTagName("td")[5];
-                td5 = tr[i].getElementsByTagName("td")[6];
-                td6 = tr[i].getElementsByTagName("td")[8];
-                td7 = tr[i].getElementsByTagName("td")[9];
+                td = tr[i].getElementsByTagName("td")[0];
+                td1 = tr[i].getElementsByTagName("td")[1];
+                td2 = tr[i].getElementsByTagName("td")[2];
+                td3 = tr[i].getElementsByTagName("td")[5];
+                td4 = tr[i].getElementsByTagName("td")[6];
+                td5 = tr[i].getElementsByTagName("td")[7];
+                td6 = tr[i].getElementsByTagName("td")[8];    
                 if (td) 
                 {
-                  if (td.innerHTML.toUpperCase().indexOf(filter) > -1 || td1.innerHTML.toUpperCase().indexOf(filter) > -1 || td2.innerHTML.toUpperCase().indexOf(filter) > -1 || td3.innerHTML.toUpperCase().indexOf(filter) > -1 || td4.innerHTML.toUpperCase().indexOf(filter) > -1 || td5.innerHTML.toUpperCase().indexOf(filter) > -1 || td6.innerHTML.toUpperCase().indexOf(filter) > -1 || td7.innerHTML.toUpperCase().indexOf(filter) > -1) 
+                  if (td.innerHTML.toUpperCase().indexOf(filter) > -1 || td1.innerHTML.toUpperCase().indexOf(filter) > -1 || td2.innerHTML.toUpperCase().indexOf(filter) > -1 || td3.innerHTML.toUpperCase().indexOf(filter) > -1 || td4.innerHTML.toUpperCase().indexOf(filter) > -1 || td5.innerHTML.toUpperCase().indexOf(filter) > -1 || td6.innerHTML.toUpperCase().indexOf(filter) > -1) 
                   {
                     tr[i].style.display = "";
                   } else {
