a101dcf2596e4ef9dedf33454107aa9037a42f10
vince.agbayani <jvinceagbayani@gmail.com>
2018-03-28 02:42:35 +0800

    added assessment chart for managers

--- resources/views/index_mg.blade.php
+++ resources/views/index_mg.blade.php
@@ -204,87 +204,12 @@
                         <h5 class="dashboard-header"><i class="fa fa-users"></i>Department overview</h5>
                         <div class="dashboard-content">
 
-                            <table class="table table-striped table-bordered">
-                                <thead>
-                                    <tr>
-                                        <td>First Name</td>
-                                        <td>Last Name</td>
-                                        <td>Email</td>
-                                        <td>Hiring Date</td>
-                                        <td>Birth Date</td>
-                                        <td>Department</td>
-                                        <td>Supervisor</td>
-                                        <td>Position</td>
-                                        <td>Manager?</td>
-                                        <td>Actions</td>
-                                    </tr>
-                                </thead>
+                           
                                 
-                                <tbody>
-                                <?php
 
-                                    /* 
-                                        1) Buffer to Extraction Area
-                                        2) Extract
-                                        3) Extracted Children to Buffer
-                                        4) Parents to List
 
-                                    */
 
-                                    $list = array();
-                                    $extraction = array();
-                                    $buffer = array();
 
-                                    array_push($buffer, $current_user);
-
-                                    while(!empty($buffer)) {
-                                        $extraction = $buffer;
-                                        $buffer = array();
-                                        foreach ($extraction as $key => $parent) {
-                                            if(!in_array($parent, $list))
-                                            {
-                                                array_push($list, $parent);
-                                                $children = $parent->subordinates()->get();
-                                                foreach ($children as $key => $child) {
-                                                    array_push($buffer, $child);
-                                                }
-                                                
-                                            }
-                                        }
-                                    }
-
-                                ?>
-                                
-                                @foreach($list as $key => $user)
-                                    <tr>
-                                        <td>{{ $user->first_name }}</td>
-                                        <td>{{ $user->last_name }}</td>
-                                        <td>{{ $user->email }}</td>
-                                        <td>{{ $user->hiring_date }}</td>
-                                        <td>{{ $user->birth_date }}</td>
-                                        <td>{{ $user->department }}</td>
-                                        @foreach($users_two as $key => $supervisor)
-                                            @if($user->supervisor_id == $supervisor->id)
-                                                 <td>{{ $supervisor->first_name }} {{ $supervisor->last_name }}</td>
-                                            @endif
-                                        @endforeach
-                                        <td>{{ $user->position }}</td>
-                                        @if ($user->manager_check==1)
-                                        <td>Yes</td>
-                                        @else
-                                        <td>No</td>
-                                        @endif
-                                        <!-- we will also add show, edit, and delete buttons -->
-                                        <td class="table-actions">
-                                            <a class="btn show-btn" data-toggle="tooltip" data-placement="bottom" title="View employee" href="{{ URL::to('users/' . $user->id) }}">
-                                                <i class="fa fa-user fa-lg"></i>
-                                            </a>
-                                        </td>
-                                        </tr>                
-                                @endforeach
-                                </tbody>
-                            </table>
-                        </div>
                     </div>
                 </div>
 
@@ -318,7 +243,118 @@
         @endforeach
     </tbody>
     </table>
-    
+
+    <?php
+
+                                    /* 
+                                        1) Buffer to Extraction Area
+                                        2) Extract
+                                        3) Extracted Children to Buffer
+                                        4) Parents to List
+
+                                    */
+
+                                    $list = array();
+                                    $extraction = array();
+                                    $buffer = array();
+
+                                    array_push($buffer, $current_user);
+
+                                    while(!empty($buffer)) {
+                                        $extraction = $buffer;
+                                        $buffer = array();
+                                        foreach ($extraction as $key => $parent) {
+                                            if(!in_array($parent, $list))
+                                            {
+                                                array_push($list, $parent);
+                                                $children = $parent->subordinates()->get();
+                                                foreach ($children as $key => $child) {
+                                                    array_push($buffer, $child);
+                                                }
+                                                
+                                            }
+                                        }
+                                    }
+
+                                ?>
+                                
+                        </div>
+
+                        <?php
+                        $init_criteria_label = array();
+                        $init_criteria_score = array();
+
+                        foreach($grades as $key=>$value)
+                        {
+                            if($value->supervisor_id==$current_id)
+                            {
+                                if(in_array($value->criteria, $init_criteria_label)==false)
+                                {
+                                    array_push($init_criteria_score, $value->grade);
+                                    array_push($init_criteria_label, $value->criteria);
+                                }
+                                else
+                                {
+                                    $key = $key = array_search($value->criteria, $init_criteria_label);
+                                    $init_criteria_score[$key]+=$value->grade;
+                                }
+                            }
+                        }
+                        ?>
+                        <canvas id="eum_criteria_chart" width="100px" height="100px"></canvas>
+                        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
+                        <script type="text/javascript">
+
+                            var score_data_all = <?php echo json_encode($init_criteria_score)?>;
+                            var labels_all = <?php echo json_encode($init_criteria_label)?>;
+
+
+                            Chart.defaults.global.maintainAspectRatio = false;
+                            var ctx = document.getElementById("eum_criteria_chart").getContext('2d');
+                            var eum_criteria_chart = new Chart(ctx, {
+                                type: 'horizontalBar',
+                                data: {
+                                    labels: labels_all,
+                                    datasets: [{
+                                        label: 'Skill Level by Percentage',
+                                        data: score_data_all,
+                                        backgroundColor: [
+                                            'rgba(255, 99, 132, 0.5)',
+                                            'rgba(54, 162, 235, 0.5)',
+                                            'rgba(255, 206, 86, 0.5)',
+                                            'rgba(75, 192, 192, 0.5)',
+                                            'rgba(153, 102, 255, 0.5)',
+                                            'rgba(255, 159, 64, 0.5)'
+                                        ],
+                                        borderColor: [
+                                            'rgba(255,99,132,1)',
+                                            'rgba(54, 162, 235, 1)',
+                                            'rgba(255, 206, 86, 1)',
+                                            'rgba(75, 192, 192, 1)',
+                                            'rgba(153, 102, 255, 1)',
+                                            'rgba(255, 159, 64, 1)'
+                                        ],
+                                        borderWidth: 1
+                                    }]
+                                },
+                                options: {
+                                    scales: {
+                                        yAxes: [{
+                                            ticks: {
+                                                beginAtZero:true
+                                            }
+                                        }],
+                                        xAxes: [{
+                                            ticks: {
+                                                beginAtZero:true
+                                            }
+                                        }]
+
+                                    }
+                                }
+                            });
+                        </script>
+
 @endsection
 
 <style>
