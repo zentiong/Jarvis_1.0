861c85cf84faad655838e5c56b486beca9b99bbb
Stephen Wenceslao <stephenwenceslao@gmail.com>
2018-03-29 22:24:47 +0800

    Prettify positions/id

--- resources/views/positions/show.blade.php
+++ resources/views/positions/show.blade.php
@@ -10,34 +10,36 @@
             </div>
             <a href="{{ URL::to('positions') }}" class="btn cancel-btn">Back to All Positions</a>
         </section>
-        <section>
-             <div class="text-center flex-column-center">
-                 @foreach($users as $key=>$user)
-                    <?php 
-                        $check = $user->profile_photo;
-                        if($check!=null)
-                        {
-                            $pp = asset( 'images/profile_photos/'.$user->profile_photo);
-                        } 
-                        else 
-                        {
-                            $pp = asset( 'images/profile_photos/default.png');     
-                        }
-                    ?>
-                    <div>
-                        <div class="img-circle profile-picture" style="background-image: url('{{ $pp }}')" alt="Your profile picture"></div>
+        <section class="container flex-column-center employees-positions">
+            @foreach($users as $key=>$user)
+                <?php 
+                    $check = $user->profile_photo;
+                    if($check!=null)
+                    {
+                        $pp = asset( 'images/profile_photos/'.$user->profile_photo);
+                    } 
+                    else 
+                    {
+                        $pp = asset( 'images/profile_photos/default.png');     
+                    }
+                ?>
+                <div class="flex-row-center">
+                    <div class="img-circle profile-picture" style="background-image: url('{{ $pp }}');" alt="Your profile picture"></div>
+                    <div class="flex-column-center">
+                        <div>
+                            <strong>{{$user->first_name}} {{$user->last_name}}</strong> 
+                        </div>
+                        <div>
+                            {{$user->department}} 
+                        </div>
                     </div>
                     <div>
-                        {{$user->first_name}} {{$user->last_name}} 
-                        {{$user->department}} 
+                        <a href="">LINK TO PROFILE</a>
                     </div>
-                    
-                @endforeach
-            </div>
+                </div>
+            @endforeach
         </section>
     </main>
-
-   
 
 @endsection
 
